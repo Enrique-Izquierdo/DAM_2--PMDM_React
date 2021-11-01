c33ea1671c42cfd7c84414c16b4754d23af42dd9
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-11-02 00:04:22 +0100

    Ejercicio 5

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -4,9 +4,13 @@ import React from "react";
 class Contenido extends React.Component{
     render(){
         return (
-            <div>
-
-            </div>
+            <form>
+                <label for="fnombre">Nombre: </label><input type="text" id="fnombre" name="nombre"/><br/>
+                <label for="fapellidos">Apellidos: </label><input type="text" id="fapellidos" name="apellidos"/><br/>
+                <label for="fapellidos">A continuación puede indicar cualquier información de interés: </label><br/>
+                <textarea name="comentarios" rows="10" cols="60"/><br/>
+                <input type="submit" value="Enviar"/>
+            </form>
         );
     }
 }
