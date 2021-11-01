a37e3b275fc4e8b5ea15556f9a0a9f2d34122dc2
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-11-01 23:26:12 +0100

    Ejercicio 4

---
+++ src/components/cabecera.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Cabecera extends React.Component{
+    render(){
+        return (
+            <div class="encabezado">
+                <img src="https://th.bing.com/th/id/OIP.kADbjwlk41ra5J5sgabdSwHaFj?pid=ImgDet&rs=1" alt="Vista de Machu Picchu y Huayna Picchu" width="400" height="300" title="Machu Picchu"/>
+            </div>
+        ) ;
+    }
+}
+
+export default Cabecera;
