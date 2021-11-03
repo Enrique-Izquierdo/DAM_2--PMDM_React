800a0c2346c3ce94745aa417848f7629bf0bed21
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-11-04 00:22:11 +0100

    Ejercicio 5

--- src/components/principal.js
+++ src/components/principal.js
@@ -13,7 +13,9 @@ class Principal extends React.Component{
             <body>
                 <Cabecera/>
                 <Contenido titulo='Contenido 1' parrafo1='Parrafo 1A' parrafo2='Parrafo 1B'/>
-                <Contenido titulo='Contenido 2' parrafo1='Parrafo 2A' parrafo2='Parrafo 2B'/>
+                <Contenido titulo='Contenido 2' 
+                    parrafo1={<img src={require('./img/paisaje.jpg').default} alt='paisaje' width='100%'/>} 
+                    parrafo2='Parrafo 2B'/>
                 <Contenido titulo='Contenido 3' parrafo1='Parrafo 3A' parrafo2='Parrafo 3B'/>
             </body>
         );
