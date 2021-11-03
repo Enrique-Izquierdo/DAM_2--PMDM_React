af71be8eebfcb1603b6aa54e0c090dc1e9288d18
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-11-04 00:12:06 +0100

    Ejercicio 4

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -2,36 +2,36 @@
 import React from "react";
 
 //Declaramos el componente de clase Contenido
-//Parametrizando mediante state
+//Parametrizando mediante prop
 class Contenido extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            titulo: 'Esto es un título',
-            parrafo1: 'Esto es el parrafo 1',
-            parrafo2: 'Esto es el parrafo 2'
-        };
-    }
     render(){
         return (
-            <header className='stContenido'>
-                <h2>{this.state.titulo}</h2>
-                <p>{this.state.parrafo1}</p>
-                <p>{this.state.parrafo2}</p>
-            </header>
+            <main className='stContenido'>
+                <h2>{this.props.titulo}</h2>
+                <p>{this.props.parrafo1}</p>
+                <p>{this.props.parrafo2}</p>
+            </main>
         );
     }
 }
 
-//Parametrizando mediante prop
+//Parametrizando mediante state
 // class Contenido extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             titulo: 'Esto es un título',
+//             parrafo1: 'Esto es el parrafo 1',
+//             parrafo2: 'Esto es el parrafo 2'
+//         };
+//     }
 //     render(){
 //         return (
-//             <main className='stContenido'>
-//                 <h2>{this.props.titulo}</h2>
-//                 <p>{this.props.parrafo1}</p>
-//                 <p>{this.props.parrafo2}</p>
-//             </main>
+//             <header className='stContenido'>
+//                 <h2>{this.state.titulo}</h2>
+//                 <p>{this.state.parrafo1}</p>
+//                 <p>{this.state.parrafo2}</p>
+//             </header>
 //         );
 //     }
 // }
--- src/components/principal.js
+++ src/components/principal.js
@@ -6,26 +6,27 @@ import Contenido from './contenido';
 
 //Declaramos el componente de clase Principal que contiene los componentes
 //de clase Cabecera y Contenido.
-//Parametrizando mediante state
+//Parametrizando mediante prop
 class Principal extends React.Component{
     render(){
         return (
             <body>
                 <Cabecera/>
-                <Contenido/>
+                <Contenido titulo='Contenido 1' parrafo1='Parrafo 1A' parrafo2='Parrafo 1B'/>
+                <Contenido titulo='Contenido 2' parrafo1='Parrafo 2A' parrafo2='Parrafo 2B'/>
+                <Contenido titulo='Contenido 3' parrafo1='Parrafo 3A' parrafo2='Parrafo 3B'/>
             </body>
         );
     }
 }
 
-//Parametrizando mediante prop
+//Parametrizando mediante state
 // class Principal extends React.Component{
 //     render(){
 //         return (
 //             <body>
 //                 <Cabecera/>
-//                 <Contenido titulo='Esto es un título' parrafo1='Esto es el parrafo 1' 
-//                      parrafo2='Esto es el parrafo 2'/>
+//                 <Contenido/>
 //             </body>
 //         );
 //     }
