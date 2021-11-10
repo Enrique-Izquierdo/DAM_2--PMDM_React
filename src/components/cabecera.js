ddeb60e747b3521a595fe206e64625f8554d304a
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-11-10 16:29:28 +0100

    preparación my-app

---
+++ src/components/cabecera.js
@@ -0,0 +1,11 @@
+//Importamos la libreria React para poder declarar los componentes de clase.
+import React from 'react';
+
+//Declaramos el componente de clase Cabecera
+class Cabecera extends React.Component{
+    render(){
+        return ;
+    }
+}
+//Exportamos el componente de clase Cabecera
+export default Cabecera;
