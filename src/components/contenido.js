f7cc05df7f34f10157566e99ea669b90007e505a
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-12-07 11:05:43 +0100

    Ejercicio 2

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -7,7 +7,12 @@ import React from 'react';
 class Contenido extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            titulo: "Actividades: Eventos React",
+            texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur lorem donec massa sapien faucibus et. "
+        };
         this.handleClick = this.handleClick.bind(this);
+        this.cambiarTexto = this.cambiarTexto.bind(this)
     }
 
     handleClick(event){
@@ -16,11 +21,21 @@ class Contenido extends React.Component {
         event.target.style.backgroundColor="black";
     }
 
+    cambiarTexto(){        
+        this.setState({
+            texto: ""
+        });        
+    }
+
     render() {
         return (
             <header className='stCabecera'>
-                <h1 onClick={this.handleClick}>Actividades: Eventos React</h1>
-
+                <h1 onClick={this.handleClick}>{this.state.titulo}</h1>
+                <p onClick={this.cambiarTexto}>{this.state.texto}</p>
+                <p>Eros in cursus turpis massa tincidunt dui ut. Id diam maecenas ultricies mi. Dignissim diam quis enim lobortis scelerisque. Eu volutpat odio facilisis mauris sit. </p>
+                <p>Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Quis risus sed vulputate odio ut enim blandit. Nunc mattis enim ut tellus elementum sagittis vitae et. </p>
+                <p>Amet luctus venenatis lectus magna fringilla urna. Ultricies integer quis auctor elit sed vulputate. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. </p>
+                <p>Integer eget aliquet nibh praesent tristique magna sit amet purus. Urna neque viverra justo nec ultrices. Aliquet nibh praesent tristique magna.</p>
             </header>
         );
     }
