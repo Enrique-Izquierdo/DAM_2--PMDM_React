91bb931435e1126a4f97e7ed44cbc76e7c07372d
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-12-07 10:32:16 +0100

    ejercicio 1

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -2,19 +2,29 @@
 import React from 'react';
 
 
-// Creamos el componente de clase "Contenido" que hereda de la clase React.Component, y
-// tiene un método render que devuelve...
+// Creamos el componente de clase "Contenido" que hereda de la clase
+// React.Component, y tiene un método render que devuelve...
 class Contenido extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event){
+        event.preventDefault();
+        event.target.style.color="red";
+        event.target.style.backgroundColor="black";
+    }
+
     render() {
         return (
             <header className='stCabecera'>
-                <h1>Actividades: Eventos React</h1>
+                <h1 onClick={this.handleClick}>Actividades: Eventos React</h1>
 
             </header>
         );
     }
 }
-
 
 
 // Exportamos por defecto el contenido de clase "Contenido"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // Importamos la clase React de la librería 'react' y 
-// la clase ReactDOM de la librería 'react-dom'
+// la clase ReactDOM de la librería 'react-dom'.
 import React from "react";
 import ReactDOM from "react-dom";
 // Importamos el componente de clase Principal del archivo 'principal.js',
-// ubicado en la dirección relativa ./components/principal.js
+// ubicado en la dirección relativa ./components/principal.js.
 import Principal from './components/principal.js';
 
 // Creamos el componente de clase "Index" que hereda de la clase "React", y
-// que contiene el método render() que devuelve el componente de clase "Principal"
+// que contiene el método render() que devuelve el componente de clase "Principal".
 class Index extends React.Component {
     render(){
         return <Principal/>;        
@@ -15,5 +15,5 @@ class Index extends React.Component {
 }
 
 // LLamamos al método "render()" de la clase ReactDOM para renderizar el componente 
-// de clase 'Index' en el elemento del DOM 'root', ambos pasados por parámetro
+// de clase 'Index' en el elemento del DOM 'root', ambos pasados por parámetro.
 ReactDOM.render(<Index/>, document.getElementById('root'));
