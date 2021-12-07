9dbb517f9496d807f82f4f16f50254519dea56a3
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-12-07 11:56:43 +0100

    Ejercicio 4

--- src/components/contenido.js
+++ src/components/contenido.js
@@ -9,14 +9,14 @@ class Contenido extends React.Component {
         super(props);
         this.state = {
             titulo: "Actividades: Eventos React",
-            texto1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur lorem donec massa sapien faucibus et. ",
+            texto1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur lorem donec massa sapien faucibus et. Eros in cursus turpis massa tincidunt dui ut. Id diam maecenas ultricies mi. Dignissim diam quis enim lobortis scelerisque. Eu volutpat odio facilisis mauris sit. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Quis risus sed vulputate odio ut enim blandit. Nunc mattis enim ut tellus elementum sagittis vitae et. Amet luctus venenatis lectus magna fringilla urna. Ultricies integer quis auctor elit sed vulputate. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Integer eget aliquet nibh praesent tristique magna sit amet purus. Urna neque viverra justo nec ultrices. Aliquet nibh praesent tristique magna.",
             texto2: "Eros in cursus turpis massa tincidunt dui ut. Id diam maecenas ultricies mi. Dignissim diam quis enim lobortis scelerisque. Eu volutpat odio facilisis mauris sit.",
             texto3: "Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Quis risus sed vulputate odio ut enim blandit. Nunc mattis enim ut tellus elementum sagittis vitae et.",
             texto4: "Amet luctus venenatis lectus magna fringilla urna. Ultricies integer quis auctor elit sed vulputate. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis.",
             texto5: "Integer eget aliquet nibh praesent tristique magna sit amet purus. Urna neque viverra justo nec ultrices. Aliquet nibh praesent tristique magna."
         };
         this.handleClick = this.handleClick.bind(this);
-        this.cambiarTexto1 = this.cambiarTexto1.bind(this);
+        this.cambiarEstilo = this.cambiarEstilo.bind(this);
         this.cambiarTexto2 = this.cambiarTexto2.bind(this);
         this.cambiarTexto3 = this.cambiarTexto3.bind(this);
         this.cambiarTexto4 = this.cambiarTexto4.bind(this);
@@ -29,8 +29,8 @@ class Contenido extends React.Component {
         event.target.style.backgroundColor="black";
     }
 
-    cambiarTexto1(){        
-        this.setState({ texto1: "" });        
+    cambiarEstilo(event){  
+        this.state.texto1.length<300 ? event.target.style.fontSize = "30px" : event.target.style.textDecoration = "underline";
     }
 
     cambiarTexto2(){
@@ -53,7 +53,7 @@ class Contenido extends React.Component {
         return (
             <div className='stDiv'>
                 <h1 onClick={this.handleClick}>{this.state.titulo}</h1>                
-                <p onClick={this.cambiarTexto1}>{this.state.texto1}</p>
+                <p onClick={this.cambiarEstilo}>{this.state.texto1}</p>
                 <p onClick={this.cambiarTexto2}>{this.state.texto2}</p>
                 <p onClick={this.cambiarTexto3}>{this.state.texto3}</p>
                 <p onClick={this.cambiarTexto4}>{this.state.texto4}</p>
