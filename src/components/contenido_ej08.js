10240c1e0949fcf200574b55a8e25b11fbd1e2a9
EnriqueIzquierdo <enizji@floridauniversitaria.es>
2021-12-12 12:41:58 +0100

    ejercicio 8 con dudas comentadas

--- src/components/contenido_ej08.js
+++ src/components/contenido_ej08.js
@@ -22,13 +22,14 @@ const Contenido = () => {
 
     return (
         <div>
+            <p>¿Por que "onClick" requiere doble click? ¿Por que en el operador ternario los indices correspondentes a la matriz de fotos son "-1" en lugar de "0", y "fotos.length" en vez de "fotos.length-1"</p>
             <img src={fotos.at(num)} alt="" width="75%"/>
             <h1>{num}</h1>
             <button onClick={ () => setNum(num>-1 ? num-- : num=fotos.length-1) }>Anterior</button>
             <button onClick={ () => setNum(num<fotos.length ? num++ : num=0) }>Siguiente</button><br/><br/><br/>
             {/* <button onClick={ () => setNum(num>0 ? num-- : num=fotos.length-1) }>Anterior</button> */}
             {/* <button onClick={ () => setNum(num<fotos.length-1 ? num++ : num=0) }>Siguiente</button> */}
-
+            <p>En este caso, "onClick" solo requiere un click.</p>
             <img src={imagen} alt="" width="75%"/><br/>
             <button onClick={ () => setImagen(fotos.at(0)) }>Anterior B</button>
             <button onClick={ () => setImagen(fotos.at(1))}>Siguiente B</button>
